85d5ab11b20d80b8b940dccb5be81d379876ff06
abmarz <aalmarzooq04@gmail.com>
2024-12-05 23:38:44 -0800

    haskell submission

--- haskell/Exercises.hs
+++ haskell/Exercises.hs
@@ -1,6 +1,17 @@
 module Exercises
     ( change,
-      firstThenApply
+      firstThenApply,
+      powers,
+      meaningfulLineCount,
+      volume,
+      surfaceArea,
+      Shape(Sphere, Box),
+      size,
+      inorder,
+      show,
+      insert,
+      contains,
+      BST(Empty)
     ) where
 
 import qualified Data.Map as Map
@@ -21,14 +32,63 @@ change amount
                 (count, newRemaining) = remaining `divMod` d
                 newCounts = Map.insert d count counts
 
--- Write your first then apply function here *Write f.map function*
--- firstThenApply :: [a] -> (a -> Bool) -> (a -> b) -> Maybe b
--- firstThenApply xs pred f = find pred xs >>= Just . f *Write f.map function*
+-- First then apply function 
+firstThenApply :: [a] -> (a -> Bool) -> (a -> b) -> Maybe b
+firstThenApply xs pred f = f <$> find pred xs
 
--- Write your infinite powers generator here
+-- Infinite powers generator function
+powers :: Integral a => a -> [a]
+powers base = map (base^) [0..]
 
--- Write your line count function here
+-- Line count function (help from Dr. Toal)
+meaningfulLineCount :: FilePath -> IO Int
+meaningfulLineCount path = do
+    contents <- readFile path
+    return $ length $ filter meaningfulLine $ lines contents
+    where
+      meaningfulLine line = not (all isSpace line) && not ("" `isPrefixOf` line)
 
--- Write your shape data type here
+-- Write your shape data type here (help from Dr. Toal)
+data Shape = 
+  Sphere Double 
+  | Box Double Double Double 
+  deriving (Eq, Show)
 
--- Write your binary search tree algebraic type here
+volume :: Shape -> Double
+volume (Sphere r) = 4 * pi * r^3 / 3
+volume (Box l w h) = l * w * h
+
+surfaceArea :: Shape -> Double
+surfaceArea (Sphere r) = 4 * pi * r^2
+surfaceArea (Box l w h) = 2 * (l * w + w * h + h * l)
+
+-- Binary search tree algebraic type function (help from built-in AI tools)
+data BST a 
+  = Empty 
+  | Node a (BST a) (BST a)
+
+size :: BST a -> Int
+size Empty = 0
+size (Node _ left right) = 1 + size left + size right
+
+inorder :: BST a -> [a]
+inorder Empty = []
+inorder (Node a left right) = inorder left ++ [a] ++ inorder right
+
+insert :: Ord a => a -> BST a -> BST a
+insert value Empty = Node value Empty Empty
+insert value (Node a left right)
+  | value < a = Node a (insert value left) right
+  | value > a = Node a left (insert value right)
+  | otherwise = Node a left right
+
+contains :: Ord a => a -> BST a -> Bool
+contains _ Empty = False
+contains value (Node a left right)
+  | value < a = contains value left
+  | value > a = contains value right
+  | otherwise = True
+
+instance Show a => Show (BST a) where
+  show Empty = ""
+  show (Node a left right) = "(" ++ show left ++ show a ++ show right ++ ")"
